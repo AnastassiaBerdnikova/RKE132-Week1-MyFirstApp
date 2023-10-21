a9bb7850b886ec35854846817b6fc267abd2482a
Anastassia Brd. <anastassia.berdnikova@gmail.com>
2023-10-21 19:28:56 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// this is a comment to my code
+
+//Rakendus küsib kasutaja sisestada oma nime
+
+Console.WriteLine("Enter you name:"); //output
+//string - sõne
+string userName = Console.ReadLine(); //input
+// = - salvesta
+
+
+//rakendus tervitav kasutajat nimepidi
+Console.WriteLine("Hello" + ", " + userName + "!"); //output
+// string interpolation
+Console.WriteLine($"Hello, {userName}!"); //output
+
